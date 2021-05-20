5512abd49e7ef98329fc0493dc6758ef6854c293
Fakhri Rafiki <68749023+fakhrirafiki@users.noreply.github.com>
2021-05-20 21:56:38 +0700

    firebase

--- src/redux/reducers/Auth.js/index.js
+++ src/redux/reducers/Auth.js/index.js
@@ -1,7 +1,14 @@
 import { USER_LOGGED_IN, USER_LOGGED_OUT } from '../../../constants/initialType';
 
-export default function Auth(state = {}, action) {
+const iniState = {
+    currentUser: {},
+    isLoading:{}
+}
+
+export default function Auth(state = iniState, action) {
     switch (action.type) {
+                case USER_LOGGED_IN:
+            return { token: action.payload };
         case USER_LOGGED_IN:
             return { token: action.payload };
         case USER_LOGGED_OUT:
